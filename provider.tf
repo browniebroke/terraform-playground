9082229633683720f5c9a5e2f921f238758130b7
Bruno Alla <alla.brunoo@gmail.com>
2023-03-17 16:51:30 +0000

    Remove profile from the AWS provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 # Configure the AWS provider
 provider "aws" {
   region = var.aws_region
-  profile = var.aws_profile_name
 }
--- variables.tf
+++ variables.tf
@@ -3,10 +3,6 @@ variable "aws_region" {
   description = "The AWS region to create resources in."
   default     = "eu-west-2"
 }
-variable "aws_profile_name" {
-  description = "The AWS profile to use."
-  default     = "perso"
-}
 
 # Networking
 variable "public_subnet_1_cidr" {
