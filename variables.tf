0513ed2a0546599d41fcece9411740b451a7ce5a
Bruno Alla <alla.brunoo@gmail.com>
2023-03-17 16:20:51 +0000

    Setup networking

--- variables.tf
+++ variables.tf
@@ -7,3 +7,26 @@ variable "aws_profile_name" {
   description = "The AWS profile to use."
   default     = "perso"
 }
+
+# Networking
+variable "public_subnet_1_cidr" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.1.0/24"
+}
+variable "public_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.2.0/24"
+}
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+  default     = "10.0.3.0/24"
+}
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Private Subnet 2"
+  default     = "10.0.4.0/24"
+}
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b"]
+}
