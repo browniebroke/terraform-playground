38fdf687aad5c4b753299fe354b064acce645430
Bruno Alla <alla.brunoo@gmail.com>
2023-03-20 18:57:31 +0000

    Add missing network_configuration to ECS service

--- modules/ecs.tf
+++ modules/ecs.tf
@@ -70,10 +70,14 @@ resource "aws_ecs_service" "production" {
   name            = "${var.ecs_cluster_name}-myapp-service"
   cluster         = aws_ecs_cluster.production.id
   task_definition = aws_ecs_task_definition.app.arn
-  iam_role        = aws_iam_role.ecs-service-role.arn
   desired_count   = var.app_count
   depends_on      = [aws_alb_listener.ecs-alb-http-listener, aws_iam_role_policy.ecs-service-role-policy]
   launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
+    security_groups = [aws_security_group.ecs.id]
+  }
 
   load_balancer {
     target_group_arn = aws_alb_target_group.default-target-group.arn
