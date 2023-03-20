62ddbd2bf3cf57bc5aeb3ba37c80666305d2d27e
Bruno Alla <alla.brunoo@gmail.com>
2023-03-20 19:07:05 +0000

    Remove SSH key & a few unused variables

--- modules/key-pair.tf
+++
@@ -1,8 +0,0 @@
-data "http" "ssh_keys" {
-  url = var.ssh_pubkey_url
-}
-
-resource "aws_key_pair" "production" {
-  key_name   = "${var.ecs_cluster_name}_key_pair"
-  public_key = split("\n", tostring(data.http.ssh_keys.response_body))[0]
-}
